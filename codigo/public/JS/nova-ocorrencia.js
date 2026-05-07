a0dde9e50072e14b323a628c8d90814c1501049b
Gabriel-Hub08 <gabriel.oliveira.1634061@sga.pucminas.br>
2026-05-06 22:09:39 -0300

    Criação da estrutura JS

---
+++ codigo/public/JS/nova-ocorrencia.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const mapa = L.map("mapa").setView([-19.9167, -43.9345], 12);
+
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: "&copy; OpenStreetMap"
+  }).addTo(mapa);
+});
+
+function buscarEndereco(){
+
+}
+
+function filtrarPorTipo(){
+
+}
+
+function filtrarPorPrioridade(){
+
+}
+
+function registrarOcorrencia(){
+
+}
+
+function enviarFotos(){
+
+}
+
+function selecionarLocalizacao(){
+
+}
+
+function cancelarOcorrencia(){
+
+}
+
+ 
